bc8c116d0f4094db1e0370521f3ce02ff8d29857
teagh82 <teagh8237@gmail.com>
2020-08-29 02:35:37 +0900

    kh-동적 웹페이지 까지

---
+++ nodejs/syntax/string.js
@@ -0,0 +1,2 @@
+console.log('1' + '1');
+console.log('나는 기현이고 나는 김기현이다. 기현은 22살이다.'.length);
---
+++ nodejs/syntax/variable.js
@@ -0,0 +1,12 @@
+var a = 1;
+console.log(a);
+a = 2;
+console.log(a);
+// 1 = 2; comment 에러남 숫자 1은 항상 1이니까
+
+var name = 'kihyeon';
+var letter = 'Dear ' + name + 
+' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
++ name + ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa egoing qui officia deserunt mollit anim id est laborum. '
++ name;
+console.log(letter);
